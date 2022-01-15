837c12a9371dedc4f7db169196b09a57c22e7752
Shakiba448 <shakiba448@gmail.com>
2022-01-15 21:31:06 +0600

    Final Deploy

--- pages/viewCharacter/index.js
+++ pages/viewCharacter/index.js
@@ -9,22 +9,33 @@ const ViewCharacter = () => {
 
   const router = useRouter();
 
-  if (character.length === 0) {
-    router.push("/");
-  }
-
   return (
     <Layout title="View Character">
-      <div className="flex mt-3 justify-center ">
-        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Total item added : {character.length}
-        </button>
-      </div>
-      <div className="flex flex-row flex-wrap justify-center">
-        {character.map((item) => (
-          <Cards key={item.id} item={item} />
-        ))}
-      </div>
+      {character.length !== 0 ? (
+        <>
+          {" "}
+          <div className="flex mt-3 justify-center ">
+            <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              Total item added : {character.length}
+            </button>
+          </div>
+          <div className="flex flex-row flex-wrap justify-center">
+            {character.map((item) => (
+              <Cards key={item.id} item={item} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="flex flex-col justify-center items-center h-screen">
+          <h1 className="text-4xl mb-3">You have no added person yet!</h1>
+          <button
+            onClick={() => router.back()}
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          >
+            Go Back
+          </button>
+        </div>
+      )}
     </Layout>
   );
 };
